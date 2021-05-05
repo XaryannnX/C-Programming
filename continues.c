002cc23db3ffe86faef12333f14f1cc35ebd540f
Aryan Kumar <aryankumar@Aryans-MacBook-Air.local>
2021-05-05 19:26:48 +0530

    John

---
+++ continues.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,F;
+    printf("Enter the value of temprature\n");
+    scanf("%f", &a);
+
+   F=(a*9/5)+32;
+
+ 
+ printf("The temprature in farenheight is=%f", F);
+
+
+    
+    return 0;
+
+}
